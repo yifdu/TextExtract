10c2bfb51537421630ca1acf9a1435fec784e160
yifdu <17210240004@fudan.edu.cn>
2020-04-24 03:09:23 +0800

    start BiLSTM

--- README.md
+++ README.md
@@ -10,13 +10,12 @@ Model:
 
 | Task     | Datasets| vocabulary |Model                   |Precision|Recall|F1 |
 |----------|---------|------------|------------------------|---------|------|---|
-|          |         |            |TFIDF                   |   0.170      |  0.285 | 0.213  |
-|          |         |            |TextRank                |   0.186      |  0.311 |  0.233 |
-|          |         |            |TextRank_withMultiWindow|   0.190      |  0.318 |  0.238 |
-|          |         |            |TextRank_withTitle      |   0.180      |  0.301 |  0.225 |
-|          |         |            |LDA                     |   0.053      |  0.088 |  0.066 |
-|          |         |            |LSI                     |   0.137      |  0.230 |  0.172 |
+|          |         |            |TFIDF                   |   0.224      |  0.225 | 0.225  |
+|          |         |            |TextRank                |   0.251      |  0.252 |  0.252 |
+|          |         |            |TextRank_withMultiWindow|   0.255      |  0.256 |  0.256 |
+|          |         |            |TextRank_withTitle      |   0.246      |  0.246 |  0.246 |
+|          |         |            |LDA                     |   0.069      |  0.070 |  0.069 |
+|          |         |            |LSI                     |   0.192      |  0.192 |  0.192 |
 |          |         |            |BM25                    |         |      |   |
-|          |         |            |KeyProcessor            |         |      |   |
-|          |         |            |KeyGraph                |   0.182      | 0.304  | 0.228  |
+|          |         |            |KeyGraph                |   0.244      | 0.244  | 0.244  |
 参考文献:https://github.com/topics/keyword-extraction?l=python
--- textextract/BiLSTM.py
+++ textextract/BiLSTM.py
@@ -1,18 +1,48 @@
 import pandas as pd
 import gensim
+import gensim
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import models, layers, optimizers, losses, metrics
 
 
-
-class bilstm:
-    def __init__(self,vector_dim):
-        self.vector_dim=vector_dim
-        word2vec=gensim.models.KeyedVectors.load_word2vec_format('wiki.zh.text.vector',binary=False)
-        embedding_matrix=np.zeros((len(word2vec.wv.vocab),vector_dim))
-        for i in range(len(word2vec.wv.vocab)):
-            embedding_vector=word2vec.wv[word2vec.wv.index2word[i]]
+class bilstm(models.Model):
+    def __init__(self, input_shape, w2v_filename='./Data/wiki.zh.text.vector',vector_dim=400, hidden_size=256, wv_trainable=False):
+        super(bilstm, self).__init__()
+        self.vector_dim = vector_dim
+        self.wv_trainable = wv_trainable
+        self.hidden_size = hidden_size
+        word2vec = gensim.models.KeyedVectors.load_word2vec_format(w2v_filename, binary=False)
+        self.vocab_size = len(word2vec.wv.vocab)
+        embedding_matrix = np.zeros((len(word2vec.wv.vocab), vector_dim))
+        for i in range(self.vocab_size):
+            embedding_vector = word2vec.wv[word2vec.wv.index2word[i]]
             if embedding_vector is not None:
-                embedding_matrix[i]=embedding_vector
+                embedding_matrix[i] = embedding_vector
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, self.vector_dim,
+                                                   embeddings_initializer=tf.keras.initializers.Constant(
+                                                       embedding_matrix), trainable=self.wv_trainable)
+        self.lstm = layers.Bidirectional(layers.LSTM(self.hidden_size, dropout=0.5, return_sequences=True))
+        self.fc1 = layers.TimeDistributed(layers.Dense(64, activation='relu'))
+        self.fc2 = layers.Dense(1, activation='sigmoid')
+        self.build(input_shape)
 
-        saved_embeddings=tf.constant(embedding_matrix)
-        self.embedding=tf.Variable(initial_value=saved_embeddings,trainable=False)
+    def build(self, input_shape):
+        inputs = tf.keras.Input(shape=input_shape, dtype=tf.float32)
+
+        embedding_outs = self.embedding(inputs)
+
+        lstm_outs = self.lstm(embedding_outs)
+        fc1_outs = self.fc1(lstm_outs)
+        fc1_outs = tf.reshape(fc1_outs, (-1, 64))
+        outs = self.fc2(fc1_outs)
+        self.built = True
+        return outs
+
+    def call(self, inputs):
+        embedding_outs = self.embedding(inputs)
+        lstm_outs = self.lstm(embedding_outs)
+        fc1_outs = self.fc1(lstm_outs)
+        fc1_outs = tf.reshape(fc1_outs, (-1, 64))
+        outs = self.fc2(fc1_outs)
+        return outs
